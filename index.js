98d022668b578606c560fc8c57e18b7eac548f65
Leslie Harden Cooper <lesliehcooper@gmail.com>
2023-02-23 13:07:52 -0600

    All tests passed

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let num1, num2, num3, num4, multiply, random, mod, max;
+
+num1 = 31;
+num2 = 2;
+num3 = 29
+num4 = 5;
+
+multiply = num1*num2;
+
+mod = num3 % num4;
+
+random = Math.floor(Math.random() * 10) + 1;
+
+max = Math.max(1, 2, 3, 20);
+
